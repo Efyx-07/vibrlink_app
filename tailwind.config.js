f3c6adb52166d9ddf96f4a35a59e35f10caba9d3
François-Xavier Savary <fx.savary@orange.fr>
2025-05-15 15:40:29 +0200

    basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.{js,ts,jsx,tsx,mdx}'];
+export const theme = {
+  extend: {
+    colors: {
+      darkColor: 'var(--darkColor)',
+      darkColorRelief: 'var(--darkColorRelief)',
+      whiteColor: 'var(--whiteColor)',
+      accentColor: 'var(--accentColor)',
+      errorColor: 'var(--errorColor)',
+    },
+  },
+  plugins: [],
+};
