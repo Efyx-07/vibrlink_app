352ffd7db09b7ae4bb87b838b9e608c1e7c17bbf
François-Xavier Savary <fx.savary@orange.fr>
2025-05-16 09:03:42 +0200

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Navigation from '../Navigation';
 export default function Header() {
   return (
     <header className="sticky top-0 z-40 flex h-20 w-full justify-center border-b border-whiteLight bg-darkColor">
-      <div className="flex w-full max-w-[75rem] items-center justify-between px-4">
+      <div className="flex w-full max-w-[90rem] items-center justify-between px-4">
         <SiteLogo />
         <Navigation />
       </div>
