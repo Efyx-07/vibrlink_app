51326279c73d1e801c1a8113fb0dd47c483b50c9
François-Xavier Savary <fx.savary@orange.fr>
2025-05-16 10:10:33 +0200

    signup form

--- src/components/Shared/FormField/index.tsx
+++ src/components/Shared/FormField/index.tsx
@@ -4,6 +4,9 @@ interface FormFieldProps {
   addedMention?: string;
   name: string;
   type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isValid?: boolean;
 }
 
 export default function FormField({
@@ -12,16 +15,21 @@ export default function FormField({
   addedMention,
   name,
   type,
+  value,
+  onChange,
+  isValid,
 }: FormFieldProps) {
   return (
     <div className="flex flex-col gap-2">
       <label htmlFor={id}>{label}</label>
       {addedMention && <p className="text-xs opacity-50">{addedMention}</p>}
       <input
-        className="h-16 border border-whiteLight bg-darkColorRelief pl-4 outline-none focus:border-accentColor"
+        className={`h-16 border ${isValid === false ? 'border-errorColor' : ''} bg-darkColorRelief pl-4 outline-none focus:border-accentColor`}
         id={id}
         name={name}
         type={type}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
--- src/components/Signup/SignupForm/index.tsx
+++ src/components/Signup/SignupForm/index.tsx
@@ -1,16 +1,78 @@
+'use client';
+
 import FormField from '@/components/Shared/FormField';
 import Button from '@/components/Shared/Button';
+import {
+  validateData,
+  validateEmail,
+  validatePassword,
+  validateConfirmPassword,
+} from '@/utils/validateDatas';
+import { FormEvent, useEffect, useState } from 'react';
+import { register } from '@/services/auth.service';
+import { useRouter } from 'next/navigation';
 
+// Composant pour l'inscription d'un utilisateur
+// ===========================================================================================
 export default function SignupForm() {
+  const router = useRouter();
+
+  // State pour les champs du formulaire
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [isEmailValid, setEmailValid] = useState<boolean>(false);
+  const [isPasswordValid, setPasswordValid] = useState<boolean>(false);
+  const [isConfirmPasswordValid, setConfirmPasswordValid] =
+    useState<boolean>(false);
+
+  // Vérification de la validité des champs
+  useEffect(() => {
+    setEmailValid(validateEmail(email));
+    setPasswordValid(validatePassword(password));
+    setConfirmPasswordValid(validateConfirmPassword(password, confirmPassword));
+  }, [email, password, confirmPassword]);
+
+  // Fonction de soumission du formulaire
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+
+    if (
+      !validateData(email, password) ||
+      !validateConfirmPassword(password, confirmPassword)
+    ) {
+      console.error('Invalid email or password format');
+      return;
+    }
+    try {
+      await register(email, password);
+      router.push('/vl/account/login');
+    } catch (error) {
+      console.error('Error during registration: ' + error);
+    }
+  };
+  // ===========================================================================================
+
   return (
-    <form className="flex flex-col gap-8">
-      <FormField id="email" label="Email address" name="email" type="email" />
+    <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+      <FormField
+        id="email"
+        label="Email address"
+        name="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        isValid={isEmailValid}
+      />
       <FormField
         id="password"
         label="Create a password"
         addedMention="8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character"
         name="password"
         type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        isValid={isPasswordValid}
       />
       <FormField
         id="password-confirm"
@@ -18,6 +80,9 @@ export default function SignupForm() {
         addedMention="must be identical to your password"
         name="password-confirm"
         type="password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+        isValid={isConfirmPasswordValid}
       />
       <Button type="submit" label="Sign up" />
     </form>
