fbd4be0b84b533d3fcb815d3bf5a57d350c9f5bd
François-Xavier Savary <fx.savary@orange.fr>
2025-05-16 12:41:13 +0200

    header - accoutItem

---
+++ src/components/Header/MyAccountItem/index.tsx
@@ -0,0 +1,7 @@
+export default function MyAccountItem() {
+    return (
+        <div className="h-full hover:text-accentColor flex items-center cursor-pointer">
+            <p>My account</p>
+        </div>
+    )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import SiteLogo from './SiteLogo';
 import Navigation from '../Navigation';
+import MyAccountItem from './MyAccountItem';
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-40 flex h-20 w-full justify-center border-b border-whiteLight bg-darkColor">
-      <div className="flex w-full max-w-[90rem] items-center justify-between px-4">
+      <div className="h-full flex w-full max-w-[90rem] items-center justify-between px-4">
         <SiteLogo />
-        <Navigation />
+        <div className='h-full flex gap-8'>
+          <Navigation />
+          <MyAccountItem />
+        </div>
       </div>
     </header>
   );
