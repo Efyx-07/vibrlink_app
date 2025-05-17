e8054a6c0f49ce4e3e93062c4f24728a01a172f7
François-Xavier Savary <fx.savary@orange.fr>
2025-05-17 16:55:36 +0200

    release-card

--- src/components/Links/MyLinks/ReleaseCard/index.tsx
+++ src/components/Links/MyLinks/ReleaseCard/index.tsx
@@ -1,13 +1,21 @@
 import { Release } from '@/interfaces/release.interface';
+import { useRouter } from 'next/navigation';
 
 interface ReleaseCardProps {
   release: Release;
 }
 
 export default function ReleaseCard({ release }: ReleaseCardProps) {
+  const router = useRouter();
+
+  const navToReleaseToEditPage = (releaseSlug: string): void => {
+    router.push(`/vl/links/link-editor/${releaseSlug}`);
+  };
+
   return (
     <div className="w-full bg-darkColorRelief">
       <h1>{release.title}</h1>
+      <button onClick={() => navToReleaseToEditPage(release.slug)}>Edit</button>
     </div>
   );
 }
