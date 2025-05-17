c65d1195efc91b675ee9429ba67cb3b8d291ff4b
Efyx-07 <fx.savary@orange.fr>
2025-05-18 01:26:22 +0200

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,20 +11,26 @@ export default function Header() {
   // Gestion de l'état d'ouverture du Mobile Menu
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const toggleMobileMenu = (): void => setIsOpen((prev) => !prev);
+  // Gestion de l'état d'affichage de MyAccountItem en version mobile
+  const [isDisplayed, setIsDisplayed] = useState<boolean>(true);
+
+  // Alterne les états
+  const toggleMobileMenu = (): void => {
+    setIsOpen((prev) => !prev);
+    setIsDisplayed((prev) => !prev);
+  };
 
   return (
     <header className="sticky top-0 z-40 flex h-20 w-full justify-center border-b border-whiteLight bg-darkColor">
       <div className="flex h-full w-full max-w-[75rem] items-center justify-between px-4 md:px-8 2xl:max-w-[90rem]">
         <SiteLogo />
-        <div className="flex h-full gap-10 md:gap-12">
-          <div className="hidden md:block">
-            <Navigation />
-          </div>
-          {!isOpen && <MyAccountItem />}
-          <div className="md:hidden">
-            <MobileMenuIcon isOpen={isOpen} onClick={toggleMobileMenu} />
-          </div>
+        <div className="hidden md:flex md:h-full md:gap-12">
+          <Navigation />
+          <MyAccountItem />
+        </div>
+        <div className="flex h-full gap-10 md:hidden">
+          {isDisplayed && <MyAccountItem />}
+          <MobileMenuIcon isOpen={isOpen} onClick={toggleMobileMenu} />
         </div>
       </div>
       <MobilieMenu isOpen={isOpen} onCloseMenu={() => setIsOpen(false)} />
