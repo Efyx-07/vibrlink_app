0853529bfc9bf9a13dffa903f78150a72744bb8a
Efyx-07 <fx.savary@orange.fr>
2025-05-18 00:22:21 +0200

    mobile menu

--- src/components/Header/MobileMenu/index.tsx
+++ src/components/Header/MobileMenu/index.tsx
@@ -4,7 +4,9 @@ interface MobileMenuProps {
 
 export default function MobilieMenu({ isOpen }: MobileMenuProps) {
   return (
-    <div className="fixed top-20 h-[calc(100dvh-5rem)] w-full bg-darkColorRelief">
+    <div
+      className={`fixed top-20 flex h-[calc(100dvh-5rem)] w-full transform items-center bg-darkColorRelief transition-transform duration-200 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} `}
+    >
       <h1>Mobile menu</h1>
     </div>
   );
