9c92fe46352f44c079c7637ee5d183aaa459ba74
Efyx-07 <fx.savary@orange.fr>
2025-05-17 18:47:51 +0200

    release landing page

--- src/components/Links/MyLinks/ReleaseCard/index.tsx
+++ src/components/Links/MyLinks/ReleaseCard/index.tsx
@@ -1,4 +1,5 @@
 import { Release } from '@/interfaces/release.interface';
+import { openInANewTab } from '@/utils/openInANewTab';
 import { useRouter } from 'next/navigation';
 
 interface ReleaseCardProps {
@@ -12,10 +13,17 @@ export default function ReleaseCard({ release }: ReleaseCardProps) {
     router.push(`/vl/links/link-editor/${releaseSlug}`);
   };
 
+  const navToReleaseLandingPage = (releaseSlug: string): void => {
+    openInANewTab(`/${releaseSlug}`);
+  };
+
   return (
     <div className="w-full bg-darkColorRelief">
       <h1>{release.title}</h1>
       <button onClick={() => navToReleaseToEditPage(release.slug)}>Edit</button>
+      <button onClick={() => navToReleaseLandingPage(release.slug)}>
+        Landing page
+      </button>
     </div>
   );
 }
