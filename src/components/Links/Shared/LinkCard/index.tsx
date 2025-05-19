354541298d84ea804006f4ae3af840314b30903e
François-Xavier Savary <fx.savary@orange.fr>
2025-05-19 11:06:42 +0200

    link card ui

--- src/components/Links/Shared/LinkCard/index.tsx
+++ src/components/Links/Shared/LinkCard/index.tsx
@@ -3,12 +3,69 @@ import { openInANewTab } from '@/utils/openInANewTab';
 import { useRouter } from 'next/navigation';
 import { deleteLinkByReleaseId } from '@/services/release.service';
 import { useQueryClient } from '@tanstack/react-query';
+import Image from 'next/image';
+import Separator from '@/components/Shared/Separator';
 
 interface LinkCardProps {
   release: Release;
 }
 
 export default function LinkCard({ release }: LinkCardProps) {
+  return (
+    <div className="grid w-full grid-cols-[4fr_1.5fr] gap-8 border border-whiteLight bg-darkColorRelief p-4">
+      <div className="flex gap-4">
+        <div className="size-full max-w-[13rem]">
+          <Image
+            src={release.cover}
+            width={500}
+            height={500}
+            alt={release.title}
+            priority
+          />
+        </div>
+        <LinkCardInfos release={release} />
+      </div>
+      <ActionButtons release={release} />
+    </div>
+  );
+}
+
+// Composant local pour les informations de la release
+// ===========================================================================================
+interface LinkCardInfosProps {
+  release: Release;
+}
+function LinkCardInfos({ release }: LinkCardInfosProps) {
+  return (
+    <div className="w-full">
+      <div>
+        <div className="flex items-center gap-2">
+          <div className="size-full max-w-9">
+            <Image
+              src={release.artistImage}
+              width={500}
+              height={500}
+              alt={release.title}
+              priority
+            />
+          </div>
+          <p>{release.artist}</p>
+        </div>
+        <p>{release.title}</p>
+      </div>
+      <Separator />
+      <p>Creation date</p>
+    </div>
+  );
+}
+
+// Composant local pour les boutons d'action
+// ===========================================================================================
+interface ActionButtonsProps {
+  release: Release;
+}
+
+function ActionButtons({ release }: ActionButtonsProps) {
   const router = useRouter();
   const queryClient = useQueryClient();
 
@@ -28,16 +85,17 @@ export default function LinkCard({ release }: LinkCardProps) {
       console.error('Error while deleting link:', error);
     }
   };
-
   return (
-    <div className="w-full bg-darkColorRelief">
-      <h1>{release.title}</h1>
-      <div className="flex gap-2">
+    <div>
+      <div className="flex flex-col gap-2">
         <button
           className="border border-whiteColor p-2"
           onClick={() => navToLinkToEditPage(release.slug)}
         >
           Edit
+        </button>
+        <button className="border border-whiteColor p-2" onClick={deleteLink}>
+          Delete link
         </button>
         <button
           className="border border-whiteColor p-2"
@@ -45,8 +103,8 @@ export default function LinkCard({ release }: LinkCardProps) {
         >
           Landing page
         </button>
-        <button className="border border-whiteColor p-2" onClick={deleteLink}>
-          Delete link
+        <button className="border border-whiteColor p-2" onClick={() => {}}>
+          Copy album link
         </button>
       </div>
     </div>
