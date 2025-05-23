c87898173c8d7e3e01c4274d8b37c77e0f81a0f1
François-Xavier Savary <fx.savary@orange.fr>
2025-05-23 11:29:35 +0200

    doc

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -12,26 +12,43 @@ interface State {
   loadUserDataFromLocalStorage: () => Promise<void>;
 }
 
+// Store pour la gestion de l'utilisateur
+// ===========================================================================================
 const useUserStore = create<State>((set, get) => ({
   user: null,
   token: null,
   isLoggedIn: false,
+
+  // Met à jour le token et statut de connexion, sauvegarde le token dans le local-storage
+  // ===========================================================================================
   setToken: (newToken) => {
     set({ token: newToken, isLoggedIn: !!newToken });
     localStorage.setItem('token', newToken ?? '');
   },
+
+  // Sauvegarde les données user dans le local-storage
+  // ===========================================================================================
   saveUserDataInLocalStorage: () => {
     localStorage.setItem('user', JSON.stringify(get().user));
   },
+
+  // Met à jour les données utilisateur dans le store et les sauvegarde dans le local-storage
+  // ===========================================================================================
   setUserData: (user) => {
     set({ user });
     get().saveUserDataInLocalStorage();
   },
+
+  // Déconnecte l'utilisateur : supprime token et données utilisateur du localStorage et du store
+  // ===========================================================================================
   logOutUser: () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
     set({ user: null, token: null, isLoggedIn: false });
   },
+
+  // Charge les données utilisateur et le token depuis le local-storage
+  // ===========================================================================================
   loadUserDataFromLocalStorage: async () => {
     const localStorageUserData = localStorage.getItem('user');
     const token = localStorage.getItem('token');
