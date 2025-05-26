0c7dc18222be6a88a751e0e4a2812dfec6d39c5c
François-Xavier Savary <fx.savary@orange.fr>
2025-05-26 17:46:08 +0200

    delete account component

--- src/components/Account/Settings/DeleteAccount/index.tsx
+++ src/components/Account/Settings/DeleteAccount/index.tsx
@@ -1,15 +1,36 @@
 'use client';
 
 import ConfirmModal from '@/components/Shared/ConfirmModal';
+import { useDeleteAccount } from '@/hooks/useDeleteAccount';
 import { useState } from 'react';
+import useUserStore from '@/stores/userStore';
+import { User } from '@/interfaces/user.interface';
+import { useRouter } from 'next/navigation';
 
 export default function DeleteAccount() {
+  const router = useRouter();
+  const userStore = useUserStore();
+
   // State pour gérer l'ouverture et la fermeture de la modale de confirmation
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   // Gère l'ouverture et la fermeture de la modale de confirmation
   const openConfirmModal = () => setIsOpen(true);
   const closeConfirmModal = () => setIsOpen(false);
+
+  // Récupération du user et methode de déconnexion du store
+  const { user, logOutUser } = userStore;
+  const userId = user?.id as User['id'];
+  if (!userId) return;
+
+  // Utilisation du hook et la mutation pour supprimer le compte utilisateur
+  const { mutate, isPending } = useDeleteAccount();
+  const deleteAccount = async (): Promise<void> => {
+    mutate(userId);
+    closeConfirmModal();
+    logOutUser(); // Deconnecte l'utilisateur après la suppression
+    router.push('/'); // Redirige vers la page d'accueil après la suppression
+  };
   // ===========================================================================================
 
   return (
@@ -26,10 +47,10 @@ export default function DeleteAccount() {
         <ConfirmModal
           topline={`Are you sure you want to delete your account? ?`}
           message="This will definitely remove this release."
-          onConfirm={() => {}}
+          onConfirm={deleteAccount}
           onCancel={closeConfirmModal}
           icon="mdi:skull-crossbones"
-          //isLoading={isPending}
+          isLoading={isPending}
         />
       )}
     </>
