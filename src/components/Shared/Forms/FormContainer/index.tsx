a23397ea98acf71f9f7109522756a9d7f7839b4a
François-Xavier Savary <fx.savary@orange.fr>
2025-05-28 11:12:23 +0200

    [UI] scrollbar form-container

--- src/components/Shared/Forms/FormContainer/index.tsx
+++ src/components/Shared/Forms/FormContainer/index.tsx
@@ -18,7 +18,7 @@ export default function FormContainer({
   return (
     <div className="flex size-full justify-center">
       <div
-        className={`${isJustifyCenter ? 'justify-center' : ''} flex size-full max-w-[31rem] flex-col gap-8 overflow-auto lg:py-16 lg:pr-4`}
+        className={`${isJustifyCenter ? 'justify-center' : ''} scrollbar-custom flex size-full max-w-[32rem] flex-col gap-8 overflow-auto lg:py-16 lg:pr-4`}
       >
         {children}
         {addedLinkLabel && addedLinkPath && (
