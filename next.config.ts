ebef5a444c34c303b5e43b16f72c0fc3c7d05264
François-Xavier Savary <fx.savary@orange.fr>
2025-06-04 13:24:31 +0200

    proxy to test prod

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,23 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  async rewrites() {
+    return [
+      {
+        source: '/vl/user/:path*', 
+        destination: 'https://vibrlinks-backend.vercel.app/user/:path*',
+      },
+      {
+        source: '/vl/password/:path*',
+        destination: 'https://vibrlinks-backend.vercel.app/passwordRoute/:path*',
+      },
+      {
+        source: '/vl/releases/:path*',
+        destination: 'https://vibrlinks-backend.vercel.app/releasesRoute/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
