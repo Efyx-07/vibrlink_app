3342841b18e2d6ea0701f0655385a965639b6162
Efyx-07 <fx.savary@orange.fr>
2025-06-06 01:06:11 +0200

    update .env / constant for server components

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
-export const backendUrl: string | undefined =
-  process.env.NEXT_PUBLIC_BACKEND_URL;
+export const backendUrl: string | undefined = process.env.BACKEND_URL;
 export const apiUrl: string = '/vl'; // modification pour utilisation avec le proxy (voir next.config.ts)
 export const appUrl: string | undefined = process.env.NEXT_PUBLIC_APP_URL;
 
