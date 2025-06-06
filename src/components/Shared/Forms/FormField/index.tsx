207d447bb3e81d276144bbfc186aef5b14375e89
François-Xavier Savary <fx.savary@orange.fr>
2025-06-06 14:46:05 +0200

    [UI] update form-field style

--- src/components/Shared/Forms/FormField/index.tsx
+++ src/components/Shared/Forms/FormField/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Icon } from '@iconify/react';
 import VerticalSeparator from '../../Separator/VerticalSeparator';
 
@@ -28,25 +29,46 @@ export default function FormField({
   isPasswordVisible,
   onEyeClick,
 }: FormFieldProps) {
+  const [isFocused, setIsFocused] = useState(false);
+
+  const hasError = isValid === false && value !== '';
+  const showRightBorder = isFocused || hasError;
+
+  const inputClassName = `
+    size-full
+    border
+    ${hasError ? 'border-errorColor' : 'border-whiteLight'}
+    bg-darkColorRelief
+    pl-4
+    outline-none
+    focus:border-accentColor
+    ${isPasswordField && !showRightBorder ? 'border-r-transparent' : ''}
+  `;
+
   return (
     <div className="flex w-full flex-col gap-2">
       <label htmlFor={id}>{label}</label>
       {addedMention && <p className="text-xs opacity-50">{addedMention}</p>}
+
       <div className="flex h-16 w-full">
         <input
-          className={`size-full border ${isValid === false && value !== '' ? 'border-errorColor' : 'border-whiteLight'} bg-darkColorRelief pl-4 outline-none focus:border-accentColor ${isPasswordField && isValid === false && value !== '' ? 'border-r-errorColor' : 'border-r-transparent'}`}
           id={id}
           name={name}
           type={type}
           value={value}
           onChange={onChange}
           required
+          className={inputClassName}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
         />
+
         {isPasswordField && (
           <>
             <div className="h-full border-y border-y-whiteLight bg-darkColorRelief py-3">
               <VerticalSeparator />
             </div>
+
             <div className="flex size-full max-w-20 items-center justify-center border border-l-0 border-whiteLight bg-darkColorRelief">
               <Icon
                 icon={isPasswordVisible ? 'mdi:eye-off' : 'mdi:eye'}
