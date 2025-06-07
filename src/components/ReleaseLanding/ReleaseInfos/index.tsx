0f04d6739097d57a37b54509a63cc3ceb7075aee
Efyx-07 <fx.savary@orange.fr>
2025-06-08 00:14:25 +0200

    [UI] release-landing

--- src/components/ReleaseLanding/ReleaseInfos/index.tsx
+++ src/components/ReleaseLanding/ReleaseInfos/index.tsx
@@ -8,7 +8,7 @@ interface ReleaseInfosProps {
 export default function ReleaseInfos({ release }: ReleaseInfosProps) {
   return (
     <div className="sticky top-40 z-20 flex min-h-20 w-full max-w-80 flex-col justify-center gap-1 rounded-b-md bg-whiteColor p-2 text-center text-darkColor xl:relative xl:top-0 xl:max-w-none xl:bg-transparent xl:p-0 xl:text-left xl:text-whiteColor">
-      <p className="font-semibold xl:text-[2.5rem]">
+      <p className="font-semibold xl:mb-4 xl:text-[2.5rem] xl:leading-[3rem]">
         {release.artist} - {release.title}
       </p>
       <p className="text-sm xl:hidden">Choose your platform</p>
