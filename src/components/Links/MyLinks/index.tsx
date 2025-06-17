03e87f5addc3f6787b02f90f0c6659bccdce03a2
Efyx-07 <fx.savary@orange.fr>
2025-06-18 01:01:50 +0200

    new-api releases-by-slug

--- src/components/Links/MyLinks/index.tsx
+++ src/components/Links/MyLinks/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import useUserStore from '@/stores/userStore';
-import { User } from '@/interfaces/user.interface';
 import { useReleases } from '@/hooks/releases/useReleases';
 import LoadingPage from '@/components/Layout/LoadingPage';
 import LinkCard from '../Shared/LinkCard';
@@ -10,11 +8,8 @@ import EmptyList from './EmptyList';
 import { Release } from '@/interfaces/release.interface';
 
 export default function MyLinks() {
-  const userStore = useUserStore();
-  const userId: User['id'] | undefined = userStore.user?.id;
-
   // Utilise le hook useReleases pour récupérer les releases de l'utilisateur
-  const { data: releases, isLoading } = useReleases(userId);
+  const { data: releases, isLoading } = useReleases();
 
   // Inverse l'ordre des releases pour un affichage du plus récent en 1er
   const reverseReleases = (releases: readonly Release[]) => {
